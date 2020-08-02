d95fa246b490ef07ac7660d94cc79762d60500a8
LandenStephenss <turtlederp806@icloud.com>
2020-08-02 17:35:13 -0500

    First upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// powercord.styleManager.themes.forEach((f) => powercord.styleManager.disable(f.entityID))
+const { Plugin } = require('powercord/entities');
+
+module.exports = class Panikk extends Plugin {
+  constructor() {
+    super();
+  }
+
+  async startPlugin() {
+    document.body.addEventListener("keyup", this.keyup);
+  }
+
+  pluginWillUnload() {
+    document.body.removeEventListener("keyup", this.keyup);
+  }
+  keyup(event) {
+    if (event.key == 'F5') {
+      powercord.styleManager.themes.forEach((f) =>
+        powercord.styleManager.disable(f.entityID)
+      );
+    }
+  }
+};
